3ed2176520d4de35711a5b8ed8136d7d3c197c82
Walt Lungan <waltlungan@gmail.com>
2022-06-17 15:40:03 -0700

    added initials

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://ztlaw.github.io/',
-  title: 'Professional Portfolio',
+  title: 'WL',
 }
 
 const about = {
