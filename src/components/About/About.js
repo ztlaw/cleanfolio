4d493d0f9129f3f987d1541c785f681ea94d3964
Walt Lungan <waltlungan@gmail.com>
2022-06-17 14:38:59 -0700

    changed bio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
     <div className='about center'>
       {name && (
         <h1>
-          Hi, I am <span className='about__name'>{name}.</span>
+          Hello, my name is <span className='about__name'>{name}.</span>
         </h1>
       )}
 
