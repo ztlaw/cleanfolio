3b8e9068aaa5e3728f1fd02568cf37cdb2b0fb3e
Walt Lungan <waltlungan@gmail.com>
2022-06-24 20:33:16 -0700

    reset styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,14 @@
-// import './Footer.css'
+import './Footer.css'
 
-// const Footer = () => (
-//   <footer className='footer'>
-//     <a
-//       href='https://github.com/rajshekhar26/cleanfolio'
-//       className='link footer__link'
-//     >
-//       Created By Raj Shekhar
-//     </a>
-//   </footer>
-// )
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/rajshekhar26/cleanfolio'
+      className='link footer__link'
+    >
+      Created By Raj Shekhar
+    </a>
+  </footer>
+)
 
-// export default Footer
+export default Footer
