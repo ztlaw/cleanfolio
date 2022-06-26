b8d0edaef00a179f31cce91a9a5f64093f12b8a3
Walt Lungan <waltlungan@gmail.com>
2022-06-25 22:27:25 -0700

    last styles changed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://github.com/ztlaw'
       className='link footer__link'
     >
-      Created By Raj Shekhar
+      Walt Lungan
     </a>
   </footer>
 )
